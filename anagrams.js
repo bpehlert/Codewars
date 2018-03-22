a1c0babf9b78338e451e4525274a248a9d170df7
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-03-21 21:17:20 -0600

    New puzzle solved!

---
+++ anagrams.js
@@ -0,0 +1,25 @@
+// // What is an anagram? Well, two words are anagrams of each other if they both contain the same letters.
+// For example:
+// 'abba' & 'baab' == true
+// 'abba' & 'bbaa' == true
+// 'abba' & 'abbba' == false
+// Write a function that will find all the anagrams of a word from a list.
+// You will be given two inputs a word and an array with words.
+// You should return an array of all the anagrams or an empty array if there are none. For example:
+// anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+// anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+// anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+
+var words = ['aabb', 'abcd', 'bbaa', 'dada']
+var word = 'abba'
+
+function anagrams(word, words) {
+  var anagramArr = []
+  for (var i = 0; i < words.length; i++) {
+    if (word.split("").sort().toString() == words[i].split("").sort().toString()) {
+      anagramArr.push(words[i]);
+    }
+
+  }
+  return anagramArr
+}
