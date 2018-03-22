a55451c9242ac832b64a09c4ed2532dfa3ecdad4
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-03-21 22:49:58 -0600

    Final solution

--- validParentheses.js
+++ validParentheses.js
@@ -6,11 +6,8 @@
 // "("               =>  false
 // "(())((()())())"  =>  true
 
-<<<<<<< HEAD
-// var parens = ")(())"
-=======
+
 // var parens = ")(()))"
->>>>>>> 48a1dcfe3fefe2890a807024a12706678021c280
 
 function validParentheses(parens){
   if (parens[0] == ")" || parens[parens.length-1] == "(") {
