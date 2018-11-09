bcdae734d0ce0ff413eebee3e909e8c279151df4
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-11-09 15:27:36 -0700

    Not finished, added instructions

--- runoff-voting.js
+++ runoff-voting.js
@@ -1,3 +1,15 @@
+// Your task is to implement a function that calculates an election winner from a list of voter selections using an Instant Runoff Voting algorithm. If you haven't heard of IRV, here's a basic overview (slightly altered for this kata):
+
+// Each voter selects several candidates in order of preference.
+// The votes are tallied from the each voter's first choice.
+// If the first-place candidate has more than half the total votes, they win.
+// Otherwise, find the candidate who got the least votes and remove them from each person's voting list.
+// In case of a tie for least, remove all of the tying candidates.
+// In case of a complete tie between every candidate, return nil(Ruby)/None(Python)/undefined(JS).
+// Start over.
+// Continue until somebody has more than half the votes; they are the winner.
+// Your function will be given a list of voter ballots; each ballot will be a list of candidates (symbols) in descending order of preference. You should return the symbol corresponding to the winning candidate. See the default test for an example!
+
 
 // If first place has more than half of the votes = winner
 // Else: remove the last place and run again
@@ -26,7 +38,7 @@ function runoff(voters){
 
     // Function that receives votes and elimnates votes due to logic
     function clearVote (votes, firstChoice) {
-        
+
     }
 
 
