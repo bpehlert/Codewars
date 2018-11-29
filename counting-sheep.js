2b78c7cd3e7760dcd33100e29278719d23193594
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-11-29 12:22:46 -0700

    New exercise for reducing array.

---
+++ counting-sheep.js
@@ -0,0 +1,45 @@
+// Consider an array of sheep where some sheep may be missing
+// from their place. We need a function that counts the number
+// of sheep present in the array (true means present).
+
+// For example,
+
+var array1 = [
+  true,
+  true,
+  true,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  true,
+  false,
+  true,
+  false,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  false,
+  true,
+  true
+];
+
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+function countSheeps(arrayOfSheep) {
+  const number = arrayOfSheep.reduce((acc, sheep) => {
+    return sheep ? (acc += 1) : acc;
+  }, 0);
+  return number;
+}
+
+console.log(countSheeps(array1));
