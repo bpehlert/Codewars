24af7a8c19f521f6e03a04551e4ac6b7a7399e53
Benjamin Ehlert <benjamin.ehlert@gmail.com>
2019-05-02 10:18:31 -0600

    Added switch statement for battles

--- the-greatest-warrior.js
+++ the-greatest-warrior.js
@@ -5,7 +5,30 @@ function Warrior() {
   this.achievements = [];
   this.training = (description, points, minLevelReq) => {};
   this.battle = enemyLevel => {
-    if (enemyLevel <= 0 || enemyLevel > 100) return "Inavlid Level";
-    return "test";
+    if (enemyLevel > 0 && enemyLevel <= 100) {
+      const diff = enemyLevel - this.level;
+      switch (true) {
+        case diff === 0:
+          this.experience += 10;
+          return "A good fight";
+        case diff === -1:
+          this.experience += 5;
+          return "A good fight";
+        case diff <= -2:
+          return "Easy fight";
+        case diff >= 1 && diff < 5:
+          this.experience += 20 * diff * diff;
+          return "An intense fight";
+        case diff >= 5:
+          return "You've been defeated";
+        default:
+          return "didn't work";
+      }
+      return;
+    }
+    return "Invalid level";
   };
+  this.training = (description, points, minLevelReq) => {};
 }
+
+const Ben = new Warrior();
