1a3074ab450feaca8be9065f392fdd222fd40323
Benjamin Ehlert <benjamin.ehlert@gmail.com>
2019-06-10 14:54:51 -0600

    added apr calculator

---
+++ apr-calculator.js
@@ -0,0 +1,29 @@
+const apr = (loanAmount, term, annualRate, costs) => {
+  const rate = annualRate / 100 / 12;
+  const monthlyPayment = (loanAmount * rate) / (1 - Math.pow(1 + rate, -term));
+  let testRate = rate;
+  let i = 0;
+  let testResult = 0;
+  let testDiff = testRate;
+  while (i <= 100) {
+    testResult =
+      (testRate * Math.pow(1 + testRate, term)) /
+        (Math.pow(1 + testRate, term) - 1) -
+      monthlyPayment / (loanAmount - costs);
+    if (Math.abs(testResult) < 0.0000001) break;
+    if (testResult < 0) testRate += testDiff;
+    else {
+      testRate -= testDiff;
+      testDiff = testDiff / 2;
+      i++;
+    }
+  }
+  testRate = testRate * 12 * 100;
+  return testRate.toFixed(3);
+};
+
+const payment = (PR, IN, P) => {
+  const monthlyRate = IN / 100 / 12;
+  const monthly = (PR * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -P));
+  return monthly;
+};
